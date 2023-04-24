e09510c9186c83fe2784bed10b580af0ccf314c3
GauravsSharma <113518199+GauravsSharma@users.noreply.github.com>
2023-04-24 18:53:39 +0530

    Cara Online Shopping
    Another time

---
+++ main.js
@@ -0,0 +1,9 @@
+let open = document.getElementById('nav');
+let close = document.getElementById('nav');
+
+function opennav(){
+    open.style.right = "0px";
+}
+function closenav(){
+    open.style.right = "-200px";
+}
